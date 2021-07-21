ea2ac83e74aa49096214f47eca93ce3e80cd9d41
Adriano Santos <adriano.nego@gmail.com>
2021-07-21 16:09:25 -0400

    primeiro submodulo

---
+++ calc/calc.cpp
@@ -0,0 +1,5 @@
+#include "calc.h"
+
+float some(float a, float b){
+    return (a + b);
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <calc.h>
+
+int main(){
+    std::cout << "Testando alguma coisa compilada pelo cmake." << std::endl;
+    std::cout << some(12, 12.1) << std::endl;
+    return 0;
+};
