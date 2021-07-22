cfa7d9940abc6e507a506033b8d6d4c1c47c40f0
Adriano Santos <adriano.nego@gmail.com>
2021-07-22 13:42:58 -0400

    Ajustando script de configuracao

--- config.sh
+++ config.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-cmake -S . -B build
+DIR="./build"
+
+echo "Configurando o diretorio ${DIR} e executando o cmake."
+
+if [ -d "$DIR" ]; then
+    cmake -S . -B build
+else
+    mkdir build && cd build
+    cmake ..
+fi
+
